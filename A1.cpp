2f8b04c4e87945f05d4d8fa8e32a59bd74badcdc
ptd910 <78575000+ptd910@users.noreply.github.com>
2021-05-21 11:35:40 +0700

    Add files via upload

---
+++ A1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void f(int xval)
+{
+ int x;
+ x = xval;
+ cout<<&x<<endl;
+}
+void g(int yval)
+{
+ int y;
+ cout<<&y;
+}
+int main()
+{
+ f(7);
+ g(11);
+ return 0;
+} 
